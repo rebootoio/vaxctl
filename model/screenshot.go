a309ea3e8ce5294307b7ef416ba21fb4b281662e
Gai Radzi <gairadzi@users.noreply.github.com>
2022-04-03 17:51:46 +0300

    Open screenshot (#10)
    * Open screenshot if no filename flag was set
    
    * Add extension to temp screenshot file

--- model/screenshot.go
+++ model/screenshot.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"io/ioutil"
+	"os/exec"
+	"runtime"
 	"vaxctl/api"
 )
 
@@ -18,9 +20,31 @@ func GetScreenshot(id string, deviceUID string, filename string) error {
 		return err
 	}
 
-	err = ioutil.WriteFile(filename, responseData, 0644)
-	if err != nil {
-		return err
+	if filename != "" {
+		err = ioutil.WriteFile(filename, responseData, 0644)
+		if err != nil {
+			return err
+		}
+	} else {
+		file, err := ioutil.TempFile("", "vaxctl-screenshot*.png")
+		if err != nil {
+			return err
+		}
+		_, err = file.Write(responseData)
+		if err != nil {
+			return err
+		}
+		var openCommand string
+		if runtime.GOOS == "darwin" {
+			openCommand = "open"
+		} else if runtime.GOOS == "linux" {
+			openCommand = "display"
+		}
+		cmd := exec.Command(openCommand, file.Name())
+		err = cmd.Run()
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
